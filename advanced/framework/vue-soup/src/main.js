dbba37fc4723b1845b39e090e2bf56e29a669970
eduabi <eduabi@163.com>
2018-10-11 21:36:39 +0800

    vue拖动指令

--- advanced/framework/vue-soup/src/main.js
+++ advanced/framework/vue-soup/src/main.js
@@ -8,10 +8,10 @@ import 'muse-ui/dist/theme-carbon.css'
 import * as filters from './util/filter'
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
+require('./directives');
 Vue.use(iView);
 Vue.use(MuseUI)
 Vue.config.productionTip = false
-
 Object.keys(filters).forEach(k => Vue.filter(k, filters[k])) //注册过滤器
 /* eslint-disable no-new */
 new Vue({
--- advanced/framework/vue-soup/src/pages/test/index.vue
+++ advanced/framework/vue-soup/src/pages/test/index.vue
@@ -9,8 +9,9 @@
         <TabPane label="标签三" name="name3">标签三的内容</TabPane>
     </Tabs> -->
         <!-- <DatePicker type="datetimerange" placeholder="Select date and time" style="width: 300px"></DatePicker> -->
-        <sliderTest :initNumList="value1"></sliderTest>
-        <sliderTest :initNumList="value2"></sliderTest>
+        <!-- <sliderTest :initNumList="value1"></sliderTest>
+        <sliderTest :initNumList="value2"></sliderTest> -->
+        <div class="myDragdiv" v-drag="{age:'sdfsf'}"></div>
     </div>
 </template>
 <script>
@@ -39,6 +40,11 @@ export default {
 .example {
     width: 300px;
     height: 200px;
+    .myDragdiv {
+        width: 100px;
+        height: 100px;
+        background-color:red;
+    }
 }
 </style>
 
